0a995d6a61a103e482f6332f6658b7a434dd01c3
JosephOwusu1227 <2425402344@live.gctu.edu.gh>
2026-05-31 02:46:04 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: OWUSU JOSEPH BOADI SEKYERE
+# STUDENT ID:  2425402344
+# Email: 2425402344@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Owusu Joseph Boadi Sekyere, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials OJBS in large block letters.
+    # Each letter is 12 chars wide by 10 chars high, made of its own capital letter.
+    # Each letter is followed by a 4-char wide large period (****) on the last 2 rows.
+    # Three periods (...) appear to the left of each letter.
+    # No trailing spaces on any line.
 
+    print()
+    print("...OJBS")
+    print()
+
+    print(".....OOOOOOOO..........JJJJJJJJJJJJ........BBBBBBBBBB............SSSSSSSSSS.....")
+    print("....OOOOOOOOOO..................JJJ........BBB.....BBB..........SSSSSSSSSSS.....")
+    print("...OOO......OOO.................JJJ........BBB.......BB........SSSS.............")
+    print("...OOO......OOO.................JJJ........BBB.....BBB.........SSSS.............")
+    print("...OOO......OOO.................JJJ........BBBBBBBBBB............SSSSSS.........")
+    print("...OOO......OOO.................JJJ........BBB....BBB..............SSSSSS.......")
+    print("...OOO......OOO........JJJ......JJJ........BBB.......BB................SSSS.....")
+    print("...OOO......OOO........JJJ......JJJ........BBB.......BB................SSSS.....")
+    print("....OOOOOOOOOO....**....JJJ....JJJ....**...BBB.....BBB....**...SSSSSSSSSSS....**")
+    print(".....OOOOOOOO.....**......JJJJJJ......**...BBBBBBBBBB.....**...SSSSSSSSSSS....**")
+
+    print()
 
 
 main()
